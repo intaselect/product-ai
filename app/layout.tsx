9072bded0c76132a4e1a9e95be984d972791c7c4
Mahmoud <gospstudio2030@gmail.com>
2026-05-10 19:02:32 +0300

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 
 const geistSans = Geist({
@@ -58,6 +59,7 @@ export default function RootLayout({
      <body className="min-h-full flex flex-col">
   <Navbar />
   {children}
+  <Analytics />
 </body>
     </html>
   );
